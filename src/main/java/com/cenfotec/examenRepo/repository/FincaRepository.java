22cf6fde9cb393ee271beab0b96dd341db0e7d28
dsolanof <dsolanof@ucenfotec.ac.cr>
2020-04-07 21:53:30 -0600

    Hola

---
+++ src/main/java/com/cenfotec/examenRepo/repository/FincaRepository.java
@@ -0,0 +1,12 @@
+package com.cenfotec.examenRepo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cenfotec.examenRepo.domain.Finca;
+
+public interface FincaRepository extends JpaRepository<Finca, Long> {
+	
+	
+
+}
+
