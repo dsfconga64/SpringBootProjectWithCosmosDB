232ca039c5038106aeafb53bd149de787210a49d
dsolanof <dsolanof@ucenfotec.ac.cr>
2020-04-08 15:40:12 -0600

    added service package

---
+++ src/main/java/com/cenfotec/FincaProyecto/service/ProduccionService.java
@@ -0,0 +1,5 @@
+package com.cenfotec.FincaProyecto.service;
+
+public interface ProduccionService {
+
+}
---
+++ src/main/java/com/cenfotec/FincaProyecto/service/ProduccionServiceImpl.java
@@ -0,0 +1,5 @@
+package com.cenfotec.FincaProyecto.service;
+
+public class ProduccionServiceImpl {
+
+}
