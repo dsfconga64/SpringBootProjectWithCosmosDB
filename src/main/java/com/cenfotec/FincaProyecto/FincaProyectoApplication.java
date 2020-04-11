7a53bd4a859c8fe6b92bac51972f9919bb25378e
dsolanof <dsolanof@ucenfotec.ac.cr>
2020-04-11 01:18:24 -0600

    erro fixed dependency spring issue

--- src/main/java/com/cenfotec/FincaProyecto/FincaProyectoApplication.java
+++ src/main/java/com/cenfotec/FincaProyecto/FincaProyectoApplication.java
@@ -2,9 +2,11 @@ package com.cenfotec.FincaProyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @SpringBootApplication
+@ComponentScan({ "com.cenfotec.FincaProyecto.service" })
 public class FincaProyectoApplication {
 
 	public static void main(String[] args) {
